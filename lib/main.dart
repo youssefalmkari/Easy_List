dc3017e56907d4999ad04bf7018439e4f5a80bff
Youssef Almkari <youssefalmkari@gmail.com>
2019-03-23 13:18:06 -0500

    add show material grid debug option

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/rendering.dart';
+//import 'package:flutter/rendering.dart';
 import './product_manager.dart';
 
 void main() {
-  debugPaintSizeEnabled = true;
-  debugPaintBaselinesEnabled = true;
-  debugPaintPointersEnabled = true;
+//  debugPaintSizeEnabled = true;
+//  debugPaintBaselinesEnabled = true;
+//  debugPaintPointersEnabled = true;
 
   runApp(MyApp());
 }
@@ -14,6 +14,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+//      debugShowMaterialGrid: true,
       theme: ThemeData(
           brightness: Brightness.light,
           primarySwatch: Colors.deepOrange,
