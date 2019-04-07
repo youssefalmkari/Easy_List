502e52cca09f1e9bc9c9f190fe4ebfd06807a332
Youssef Almkari <youssefalmkari@gmail.com>
2019-04-06 19:17:21 -0500

    Added burger menu.

--- lib/pages/products.dart
+++ lib/pages/products.dart
@@ -1,17 +1,29 @@
-import 'package:flutter/material.dart';
 import 'dart:async';
+
+import 'package:flutter/material.dart';
 import 'package:flutter_app/product_manager.dart';
 
 class ProductsPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return WillPopScope(
-        onWillPop: ()  {
+        onWillPop: () {
           print('Back button pressed!');
           Navigator.pop(context, false);
           return Future.value(false);
         },
         child: Scaffold(
+          drawer: Drawer(
+            child: Column(
+              children: <Widget>[
+                AppBar(
+                  automaticallyImplyLeading: false,
+                  title: Text("Choose"),
+                ),
+                ListTile(title: Text("Manage Products"),)
+              ],
+            ),
+          ),
           appBar: AppBar(
             title: Text('EasyList'),
           ),
